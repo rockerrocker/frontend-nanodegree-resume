b66f711815b806397db6f53705920a2cf01116ef
rockerrocker <rockerrocker@users.noreply.github.com>
2014-11-09 17:53:54 +0000

    Lesson 1 - as far as part 5
    prepending name and role to HTML

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,10 @@
-$("#main").append("Paulie Rockley");
+//$("#main").append("Paulie Rockley");
+
+var name = "Paul Rockley"
+var role = "Data Analyst"
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName); // Note this will appear above formattedRole.
