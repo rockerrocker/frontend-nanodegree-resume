12d2af7600871b023eff7a97c3448e9fd883107c
rockerrocker <rockerrocker@users.noreply.github.com>
2014-11-20 20:59:14 +0000

    Completed Lesson 1 (part 'Objects')

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,5 +12,33 @@ $("#header").prepend(formattedName); // Note this will appear above formattedRol
 */
 
 var skills = ["hydraulics", "painting", "sealing"];
-$("#main").append(skills);
-$("#main").append(skills[0]);
+
+var bio = {
+	"name" : "Paul Rockley",
+	"role" : "Data Analyst",
+	"contacts" : {
+		"mobile" : "085-12345678",
+		"email" : "paul@example.com"
+	},
+	"welcomeMessage" : "Hello there, welcome to my online CV",
+	"skills" : skills,
+	"bioPic" : "images/fry.jpg"
+};
+//bio.city = "Cork"
+bio["city"] = "Cork";
+
+var work = {};
+work.position = "Low";
+work.employer = "Lucky to have me";
+work.yearsWorked = 1;
+work.busCity = "Cork";
+
+var education = {};
+education["schName"] = "Miggleton";
+education["yearsAttended"] = "1998-1999";
+education["schCity"] = "Miggleton";
+
+$("#main").append(bio.name);
+$("#main").append(bio.city);
+$("#main").append(work["position"]);
+$("#main").append(education.schName);
